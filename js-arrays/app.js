55690ff7e759be9a4429c8aac186b87a22b443f7
Blake Peters <blakerdpeters@gmail.com>
2024-10-18 10:43:45 -0700

    middle of lecture

--- js-arrays/app.js
+++ js-arrays/app.js
@@ -1,2 +1,10 @@
-const myStuff = [2, 'oh hai, Mark', true]
-console.log(myStuff);
+// const myStuff = [2, 'oh hai, Mark', true]
+// console.log(myStuff);
+// mystuff[0] = 5;
+
+
+const movies = ['Die Hard', 'Alien', 'Barbie', 'Get Out'];
+for (let movie of movies) {
+    console.log(`I love ${movie}`);
+}
+

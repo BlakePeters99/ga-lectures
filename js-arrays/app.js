85aef7f0b7812380f55aa813098ed05c23888dea
Blake Peters <blakerdpeters@gmail.com>
2024-10-18 11:28:22 -0700

    js arrays lecture complete

--- js-arrays/app.js
+++ js-arrays/app.js
@@ -3,6 +3,10 @@
 // mystuff[0] = 5;
 
 
+for (let num = 0; num < 4; num ++) {
+    console.log(num);
+}
+
 const movies = ['Die Hard', 'Alien', 'Barbie', 'Get Out'];
 for (let movie of movies) {
     console.log(`I love ${movie}`);

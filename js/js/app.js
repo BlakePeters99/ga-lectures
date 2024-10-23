2c8144d4e93cee0fab7c3c0a9a5c088a9d666987
Blake Peters <blakerdpeters@gmail.com>
2024-10-23 10:36:40 -0700

    Learned a new trick

---
+++ js/js/app.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+const titleElement = document.querySelector('#main-title');
+const paragraphElement = document.querySelector('.cool');
+
+console.dir(titleElement);
+console.dir(paragraphElement);

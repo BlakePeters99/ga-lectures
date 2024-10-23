8beed75d5064823da92dfab5fb2b060b25af7485
Blake Peters <blakerdpeters@gmail.com>
2024-10-23 11:04:03 -0700

    learned about changing element font without needing to call google font site

--- js/js/app.js
+++ js/js/app.js
@@ -3,5 +3,11 @@ console.log("hello world");
 const titleElement = document.querySelector('#main-title');
 const paragraphElement = document.querySelector('.cool');
 
+titleElement.style.textAlign = 'center';
+paragraphElement.style.color = 'yellow';
+paragraphElement.textContent = 'This paragraph is cool';
+paragraphElement.style.color = 'blue';
+paragraphElement.style.fontFamily = "sans-serif";
+
 console.dir(titleElement);
 console.dir(paragraphElement);
